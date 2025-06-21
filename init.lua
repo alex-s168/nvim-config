0718e372c311ce1e2c0318e4659f47147b2456b2
Alexander Nutz <alexandernutz68@gmail.com>
2025-06-22 01:42:56 +0200

    lots of changes

--- init.lua
+++ init.lua
@@ -2,114 +2,92 @@ vim.opt.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "williamboman/mason.nvim" },
-  { "EdenEast/nightfox.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "neovim/nvim-lspconfig" },
-  {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      opts = {},
-      version = "v3.5.4"
-  },
-  { "nvim-tree/nvim-web-devicons" },
-  { "stevearc/oil.nvim" },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { 'hrsh7th/nvim-cmp' },
-  { 'saadparwaiz1/cmp_luasnip' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-  { 'hrsh7th/cmp-path' },
-  { 'hrsh7th/cmp-buffer' },
-  {
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {}
-    end
-  },
-  {
-    "willothy/nvim-cokeline",
-    dependencies = {
-      "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
-      "stevearc/resession.nvim"       -- Optional, for persistent history
+    -- visuals --
+    { "EdenEast/nightfox.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {},
+        version = "v3.5.4"
     },
-    config = true
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2.3.0",
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp"
-  },
-  { 'davvid/telescope-git-grep.nvim' },
-  {
-    "nvim-java/nvim-java",
-    dependencies = {
-      { "neovim/nvim-lspconfig" }
-    }
-  },
-  { "lervag/vimtex" },
-{
-  "scalameta/nvim-metals",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  ft = { "scala", "sbt", "java" },
-  opts = function()
-    local metals_config = require("metals").bare_config()
-    metals_config.on_attach = function(client, bufnr)
-      -- your on_attach function
-    end
 
-    return metals_config
-  end,
-  config = function(self, metals_config)
-    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = self.ft,
-      callback = function()
-        require("metals").initialize_or_attach(metals_config)
-      end,
-      group = nvim_metals_group,
-    })
-  end
-}
+    -- important --
+    { "neovim/nvim-lspconfig" },
+    { 'hrsh7th/nvim-cmp' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-buffer' },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.3.0",
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+    },
+    { "saecki/live-rename.nvim" },
+
+    -- utils --
+    { "stevearc/oil.nvim" },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'davvid/telescope-git-grep.nvim' },
+    {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup {}
+        end
+    },
+    {
+        "willothy/nvim-cokeline",
+        dependencies = {
+            "nvim-lua/plenary.nvim",  -- Required for v0.4.0+
+            "stevearc/resession.nvim" -- Optional, for persistent history
+        },
+        config = true
+    },
+    {
+        "scalameta/nvim-metals",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        ft = { "scala", "sbt", "java" },
+        opts = function()
+            local metals_config = require("metals").bare_config()
+            metals_config.on_attach = function(client, bufnr)
+                -- your on_attach function
+            end
+
+            return metals_config
+        end,
+        config = function(self, metals_config)
+            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = self.ft,
+                callback = function()
+                    require("metals").initialize_or_attach(metals_config)
+                end,
+                group = nvim_metals_group,
+            })
+        end
+    },
 })
-
--- open tex:    ,lv
--- compile doc: ,ll
-vim.g.vimtex_view_method = "zathura"
-vim.g.maplocalleader = ","
-
-require('java').setup()
-
-require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
-})
-
-require("mason-lspconfig").setup()
 
 vim.cmd("colorscheme carbonfox")
 
@@ -121,14 +99,14 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 
 require("ibl").setup({
-  indent = {
-     char = "|"
-  },
-  whitespace = { highlight = { "Whitespace", "NonText" } },
-  scope = {
-    show_start = false,
-    show_end = false
-  }
+    indent = {
+        char = "|"
+    },
+    whitespace = { highlight = { "Whitespace", "NonText" } },
+    scope = {
+        show_start = false,
+        show_end = false
+    }
 })
 
 -- stolen from https://github.com/lukas-reineke/indent-blankline.nvim/discussions/664
@@ -147,18 +125,24 @@ end)
 
 require("oil").setup({})
 vim.keymap.set("n", "<C-l>", function()
-  require("oil").open()
+    require("oil").open()
 end)
 
-require('telescope').load_extension('git_grep')
-
-vim.keymap.set("n", "<C-w>", ":Telescope find_files<CR>", {})
+require("telescope").load_extension('git_grep')
+-- one esc => exit instead of double esc
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = require("telescope.actions").close,
+            },
+        },
+    },
+})
 
 local cmp = require 'cmp'
 cmp.setup {
     mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,88 +1,180 @@
-require("mason-lspconfig").setup {
-    ensure_installed = {
-        "lua_ls",
-        "kotlin_language_server",
---         "java_language_server",
-        "zls",
-        "texlab",
-    },
-}
-
-local lspconfig = require('lspconfig')
-
-
-require('lspconfig.configs').sq = {
-  default_config = {
-    cmd = {"C:/Users/Alexander.Nutz/sequencia-real/sqlsp/sqlsp.exe"},
-    filetypes = {"sq"},
-    settings = {},
-    single_file_support = true,
-  };
-}
-lspconfig.sq.setup {}
-vim.cmd([[autocmd BufRead,BufNewFile *.sq setfiletype sq]])
+local lspconfig = require("lspconfig")
 
 lspconfig.clangd.setup {}
 lspconfig.lua_ls.setup {}
 lspconfig.rust_analyzer.setup {}
 lspconfig.kotlin_language_server.setup {}
--- lspconfig.java_language_server.setup {}
 lspconfig.vls.setup {}
 lspconfig.zls.setup {}
 lspconfig.gleam.setup {}
 lspconfig.jdtls.setup {}
 lspconfig.texlab.setup {}
-
-vim.cmd([[autocmd BufRead,BufNewFile *.ua setfiletype uiua]])
-lspconfig.uiua.setup {}
+lspconfig.csharp_ls.setup {}
 
 require('lspconfig.configs').roc = {
-  default_config = {
-    cmd = {"roc_language_server"},
-    filetypes = {'roc'},
-    root_dir = lspconfig.util.root_pattern("*.roc"),
-    single_file_support = true,
-    settings = {},
-  };
+    default_config = {
+        cmd = { "roc_language_server" },
+        filetypes = { 'roc' },
+        root_dir = lspconfig.util.root_pattern("*.roc"),
+        single_file_support = true,
+        settings = {},
+    },
 }
 vim.cmd([[autocmd BufRead,BufNewFile *.roc setfiletype roc]])
 lspconfig.roc.setup {}
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- list: []{title, search, action}
+local function code_actions_menu(items)
+    local pickers = require("telescope.pickers")
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+    local finders = require("telescope.finders")
+    local conf = require('telescope.config').values
+
+    local opts = {
+        sorting_strategy = "ascending"
+    }
+    pickers.new(opts, {
+        prompt_title = "Code Actions",
+        finder = finders.new_table {
+            results = items,
+            entry_maker = function(entry)
+                return {
+                    value = entry,
+                    ordinal = entry.id..entry.search,
+                    display = "["..entry.id.."] "..entry.title,
+                }
+            end
+        },
+        sorter = conf.generic_sorter(opts),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                vim.schedule(selection.value.action)
+            end)
+            return true
+        end
+    }):find()
+end
+
+local function lspActionsAsync(withactions, finally)
+    local params = vim.lsp.util.make_range_params()
+    params.context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
+
+    vim.lsp.buf_request(0, 'textDocument/codeAction', params, function(err, actions, ctx, _)
+      if err then
+          print("Error getting code actions: ", err)
+      else
+          if actions then
+              withactions(actions)
+          end
+      end
+      finally()
+    end)
+end
+
+local function code_actions()
+    local _nextid = 1
+    local function allocid()
+        local v = _nextid
+        _nextid = _nextid + 1
+        return v
+    end
+
+    local actions = {
+        {
+            id = allocid(),
+            search = "rename",
+            title = "Refactor: Rename",
+            action = function()
+                require("live-rename").rename({ text = "", insert = true })
+            end
+        },
+        {
+            id = allocid(),
+            search = "LSP: References",
+            title = "LSP: References",
+            action = function()
+                vim.cmd([[Telescope lsp_references]])
+            end
+        }
+    }
+    local lsp_actions = {}
+
+    lspActionsAsync(function(lsp_actions_in)
+        for _,action in ipairs(lsp_actions_in) do
+            lsp_actions[action.title] = action
+        end
+    end, function()
+        for _,action in pairs(lsp_actions) do
+            local title = "LSP: "..action.title
+            table.insert(actions, {
+                id = allocid(),
+                search = title,
+                title = title,
+                action = function()
+                    vim.lsp.buf.code_action({
+                        apply = true,
+                        filter = function(ac)
+                            return ac.title == action.title
+                        end
+                    })
+                end
+            })
+        end
+        code_actions_menu(actions)
+    end)
+end
+vim.keymap.set({ 'n', 'v' }, '<space>ca', code_actions, {})
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set("n", " r", ":Telescope lsp_references<CR>", {})
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    end,
 })
+
+-- format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        local mode = vim.api.nvim_get_mode().mode
+        local filetype = vim.bo.filetype
+        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+            vim.cmd('lua vim.lsp.buf.format()')
+        else
+        end
+    end
+})
+
+vim.cmd([[autocmd BufRead,BufNewFile *.ua setfiletype uiua]])
+lspconfig.uiua.setup {}
+vim.cmd([[hi link @lsp.type.uiua_number @number]])
+vim.cmd([[hi link @lsp.type.uiua_string @string]])
+vim.cmd([[hi link @lsp.type.uiua_module @Type]])
+vim.cmd([[hi link @lsp.type.uiua_constant Constant]])
+vim.cmd([[hi @lsp.type.noadic_function guifg=#ed5e6a]])
+vim.cmd([[hi @lsp.type.monadic_function guifg=#95d16a]])
+vim.cmd([[hi @lsp.type.dyadic_function guifg=#54b0fc]])
+vim.cmd([[hi @lsp.type.triadic_function guifg=#8078f1]])
+vim.cmd([[hi @lsp.type.tetradic_function guifg=#f576d8]])
+vim.cmd([[hi @lsp.type.monadic_modifier guifg=#f0c36f]])
+vim.cmd([[hi @lsp.type.dyadic_modifier guifg=#cc6be9]])
+vim.cmd([[hi @lsp.type.triadic_modifier guifg=#F5A9B8]])
