5334fd71cb11a8a42b5f11f72aef2ef4fce676a0
Alexander Nutz <alexandernutz68@gmail.com>
2025-06-27 09:15:55 +0200

    more lsp stuff

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -53,27 +53,35 @@ local function code_actions_menu(items, second_title)
     }):find()
 end
 
-local function lspActionsAsync(bufnr, withactions, finally)
-    local params = { unpack(vim.lsp.util.make_range_params(nil, "utf-8")) }
-    params.context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
+local function asyncLspRequest(bufnr, action, params, withresult, finally)
+    -- TODO: re-add bufnr
+    vim.lsp.buf_request_all(0, action, params, function(results)
+        for client, result in ipairs(results) do
+            local err = result.err
+            local actions = result.result
 
-    vim.lsp.buf_request_all(bufnr, 'textDocument/codeAction', params, function(_, actions, _)
-        if actions then
-            withactions(actions)
+            if err then
+                -- TODO: better logging
+                print("LSP error: " .. vim.inspect(err))
+            end
+            if actions then
+                withresult(actions, client)
+            end
         end
         finally()
     end)
 end
 
+local function lspActionsAsync(bufnr, withactions, finally)
+    local params = vim.lsp.util.make_range_params(nil, "utf-8")
+    params.context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
+
+    asyncLspRequest(bufnr, "textDocument/codeAction", params, withactions, finally)
+end
+
 local function lspWorkspaceSymbolsAsync(bufnr, withsymbols, finally)
     local params = { query = "" }
-
-    vim.lsp.buf_request_all(bufnr, "workspace/symbol", params, function(err, list, _, _)
-        if list then
-            withsymbols(list)
-        end
-        finally()
-    end)
+    asyncLspRequest(bufnr, "workspace/symbol", params, withsymbols, finally)
 end
 
 local function escape_pattern(s)
@@ -224,7 +232,7 @@ local function code_actions()
         if any_not_commented then
             table.insert(actions, {
                 id = allocid(),
-                search = "comment out disable selection",
+                search = "comment out disable selection .c",
                 title = "Comment out selection",
                 action = function()
                     for i = 1, #lines do
@@ -238,7 +246,7 @@ local function code_actions()
         else
             table.insert(actions, {
                 id = allocid(),
-                search = "uncomment enable selection",
+                search = "uncomment enable selection .c",
                 title = "Uncomment selection",
                 action = function()
                     for i = 1, #lines do
@@ -251,32 +259,71 @@ local function code_actions()
         end
     end
 
+    -- TODO: disable if not lsp or treesitter identifier
     table.insert(actions, {
         id = allocid(),
         search = "rename",
-        title = "Refactor: Rename",
+        title = "Refactor: Rename .rn",
         action = function()
             require("live-rename").rename({ text = "", insert = true })
         end
     })
 
+    -- TODO: disable if not lsp or treesitter symbol
     table.insert(actions, {
         id = allocid(),
-        search = "LSP: References",
-        title = "LSP: References",
+        search = "Symbol References .ref",
+        title = "Symbol References",
         action = function()
             vim.cmd([[Telescope lsp_references]])
         end
     })
 
+    if filetype == "uiua" and selection.valid then
+        table.insert(actions, {
+            id = allocid(),
+            search = "Insert Regression Test .reg",
+            title = "Insert Regression Test",
+            action = function()
+                local path = vim.api.nvim_buf_get_name(bufnr)
+
+                local code = ""
+                local lines = vim.api.nvim_buf_get_lines(bufnr, selection.start_line, selection.end_line + 1, false)
+                for _, line in ipairs(lines) do
+                    code = code .. line .. "\n"
+                end
+
+                code = "Mod ~ \"" ..
+                    path .. "\"\nMod!({\n" .. code .. "}\n°□/⍚⊂≡(□$\"ass\\\"test failed\\\"match _\\n\"repr)\n&p)"
+
+                vim.system({ "uiua", "eval", code }, { text = true }, function(res)
+                    if res.code == 0 then
+                        local new_lines = vim.split(res.stdout, "\n", { plain = true })
+                        vim.api.nvim_buf_set_lines(bufnr, selection.end_line + 1, -1, false, new_lines)
+                    else
+                        error("failed to generate regression test: " .. res.stderr .. res.stdout)
+                    end
+                end)
+            end
+        })
+    end
+
     local lsp_actions = {}
-    lspActionsAsync(bufnr, function(lsp_actions_in)
+    local num_preferred_actions = 0
+    lspActionsAsync(bufnr, function(lsp_actions_in, clientId)
         for _, action in ipairs(lsp_actions_in) do
+            action.clientId = clientId
             lsp_actions[action.title] = action
+            if action.isPreferred then
+                num_preferred_actions = num_preferred_actions + 1
+            end
         end
     end, function()
         local lsp_disable_diagnostics = {}
         local have_lsp_disable_diagnostics = false
+
+        local preferred_action_id = _nextid
+        _nextid = _nextid + num_preferred_actions
 
         for _, action in pairs(lsp_actions) do
             if string.match(action.title, "^Disable diagnostics ") then
@@ -286,10 +333,31 @@ local function code_actions()
                 goto continue
             end
 
-            local title = "LSP: " .. action.title
+            local item_id = nil
+            if action.isPreferred then
+                item_id = preferred_action_id
+                preferred_action_id = preferred_action_id + 1
+            else
+                item_id = allocid()
+            end
+
+            local client = vim.lsp.get_client_by_id(action.clientId)
+
+            local search_extra = ""
+
+            local prefix = "LSP: "
+            if client and client.name == "typos_lsp" then
+                prefix = "typo: "
+                search_extra = search_extra .. " .ty"
+            end
+
+            local title = prefix .. action.title
+            if action.disabled then
+                title = title .. " (disabled: " .. action.disabled.reason .. ")"
+            end
             table.insert(actions, {
-                id = allocid(),
-                search = title,
+                id = item_id,
+                search = title .. search_extra,
                 title = title,
                 action = function()
                     vim.lsp.buf.code_action({
@@ -439,3 +507,19 @@ vim.cmd([[hi @lsp.type.tetradic_function guifg=#f576d8]])
 vim.cmd([[hi @lsp.type.monadic_modifier guifg=#f0c36f]])
 vim.cmd([[hi @lsp.type.dyadic_modifier guifg=#cc6be9]])
 vim.cmd([[hi @lsp.type.triadic_modifier guifg=#F5A9B8]])
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        local bufnr = args.buf
+        local clients = vim.lsp.get_active_clients({ name = "lsp_ai" })
+        if #clients == 0 then return end
+
+        local client_id = clients[1].id
+        -- Check if already attached
+        for _, c in pairs(vim.lsp.buf_get_clients(bufnr)) do
+            if c.id == client_id then return end
+        end
+
+        vim.lsp.buf_attach_client(bufnr, client_id)
+    end,
+})
